facbd1638fd4cb33280000f0334547b4a74ffebd
GongT <admin@gongt.me>
2022-02-14 04:29:16 +0800

    fix script

--- ci-build.sh
+++ ci-build.sh
@@ -6,7 +6,13 @@ export TAG=$1
 export TMPDIR="$RUNNER_TEMP"
 export REWRITE_IMAGE_NAME="build.local/dist/${TAG}"
 
+export REGISTRY_AUTH_FILE="$HOME/secrets/auth.json"
+
+echo "REGISTRY_AUTH_FILE=${REGISTRY_AUTH_FILE}" >>"$GITHUB_ENV"
 echo "SYSTEM_COMMON_CACHE=${SYSTEM_COMMON_CACHE:=$HOME/cache}" >>"$GITHUB_ENV"
+
+mkdir -p "$HOME/secrets"
+echo '{}' >"$REGISTRY_AUTH_FILE"
 
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 # shellcheck source=../common/functions-build-host.sh
